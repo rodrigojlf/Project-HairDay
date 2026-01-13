7bc210825229053aeece4188c30cff87bd03e9c1
rodrigojlf <rjorgefilho@gmail.com>
2026-01-13 11:29:47 -0300

    project setup done

---
+++ src/main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+//Configurações do dayjs
+import "./libs/dayjs.js"
+
+//CSS
+import "./styles/global.css"
+import "./styles/form.css"
+import "./styles/schedule.css"
+
+import dayjs from "dayjs"
+console.log(dayjs().format("HH:mm:ss"))
