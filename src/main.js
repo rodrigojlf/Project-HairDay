87135abeaa3d5f735460b54026418e6edad92a7d
rodrigojlf <rjorgefilho@gmail.com>
2026-01-14 19:43:20 -0300

    done form features

--- src/main.js
+++ src/main.js
@@ -8,5 +8,7 @@ import "./styles/global.css"
 import "./styles/form.css"
 import "./styles/schedule.css"
 
-import dayjs from "dayjs"
-console.log(dayjs().format("HH:mm:ss"))
+//JS
+import "./modules/form/submit.js"
+import "./modules/form/date-change.js"
+import "./modules/page-load.js"
---
+++ src/modules/form/submit.js
@@ -0,0 +1,39 @@
+import dayjs from "dayjs"
+
+const form = document.querySelector("form")
+const clientName = document.getElementById("client")
+const selectedDate = document.getElementById("date")
+
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+
+selectedDate.value = inputToday
+selectedDate.min = inputToday
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  try {
+    const name = clientName.value.trim()
+    
+    if (!name) {
+      return alert("Por favor, insira o nome do cliente.")
+    }
+
+    const hourSelected = document.querySelector(".hour-selected")
+
+    if (!hourSelected) {
+      return alert("Por favor, selecione um horário disponível.")
+    }
+
+    const [hour] = hourSelected.innerText.split(":")
+    const when = dayjs(selectedDate.value).add(hour, "hour")
+
+    const id = new Date().getTime()
+
+    
+
+  } catch (error) {
+    alert("Não foi possível enviar o formulário.")
+    console.error("Form submission error:", error)
+  }
+}
